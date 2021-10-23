802d1400b5f392f6923c8224c96b015bd25c061d
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-10-23 23:29:53 +0200

    routing linked with history aapi

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,10 +1,16 @@
 console.log("js is loaded");
 
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
 const router = async () => {
   const routes = [
     { path: "/", view: () => console.log("Viewing dashboard") },
     { path: "/posts", view: () => console.log("Viewing posts") },
     { path: "/settings", view: () => console.log("Viewing settings") },
+    { path: "/404", view: () => console.log("Viewing 404") },
   ];
 
   // test each route for potential match
@@ -14,9 +20,26 @@ const router = async () => {
       isMatch: location.pathname === route.path,
     };
   });
-  console.log(potentialMatches);
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+
+  if (!match) {
+    match = {
+      route: routes[3],
+      isMatch: true,
+    };
+  }
+
+  console.log(match.route.view());
 };
 
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
   router();
 });
